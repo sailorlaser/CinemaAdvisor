d6bee94dbdffab2a0d84cd53fbe604fb155975a2
laser <sventuhovski@gmail.com>
2021-05-10 15:52:18 +0300

    Добавьте файлы проекта.

---
+++ CinemaAdvisor/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaAdvisor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Эта программа поможет Вам подобрать фильм на вечер!");
+            Console.WriteLine("Как Вас зовут?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Здравствуйте, " + name);
+
+            Console.WriteLine("Фильм какого жанра Вы предпочитаете: комедия, триллер, драма или фантастика?");
+            String genre = Console.ReadLine();
+            if (genre == "комедия")
+            {
+                Console.WriteLine("Вы хотите посмотреть отечественный или зарубежный фильм?");
+                String country = Console.ReadLine();
+                if (country == "отечественный")
+                {
+                    Console.WriteLine("В таком случае, рекомендуем Вам посмотреть следующие фильмы:");
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    Console.WriteLine("Операция Ы");
+                    Console.WriteLine("Десантник Стёпочкин");
+                    Console.WriteLine("Иван Васильевич меняет профессию");
+                }
+                else
+                    {
+                    Console.WriteLine("В таком случае, рекомендуем Вам посмотреть следующие фильмы:");
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                    Console.WriteLine("Шутник Эндрю");
+                    Console.WriteLine("Эйс Вентура");
+                    Console.WriteLine("Маска");
+                    }
+            }
+            else if (genre == "фантастика")
+            {
+                Console.WriteLine("В таком случае, рекомендуем Вам посмотреть следующие фильмы:");
+                Console.WriteLine("Звёздные войны");
+                Console.WriteLine("Гарри Поттер");
+                Console.WriteLine("Властелин колец");
+            }
+            else if (genre == "триллер")
+            {
+                Console.WriteLine("В таком случае, рекомендуем Вам посмотреть следующие фильмы:");
+                Console.WriteLine("Супер воин");
+                Console.WriteLine("Красный убийца");
+                Console.WriteLine("Ветхая крепость");
+            }
+            else
+            {
+                Console.WriteLine("В таком случае, рекомендуем Вам посмотреть следующие фильмы:");
+                Console.WriteLine("Зелёная миля");
+                Console.WriteLine("Побег из Шоушенка");
+                Console.WriteLine("Великий");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TurtleTrainings/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SmallBasic.Library;
+
+namespace TurtleTrainings
+{
+    class Program
+    {
+        static void WriteT(int size)
+        {
+            Turtle.Angle = 0;
+            Turtle.Move(size);
+            Turtle.Angle = 270;
+            Turtle.Move(size / 3);
+            Turtle.Angle = 90;
+            Turtle.Move(size / 3 * 2);
+        }
+        static void WriteO(int size)
+        {
+            Turtle.Angle = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                Turtle.Move(size);
+                Turtle.TurnRight();
+            }
+        }
+        static void WriteR(int size)
+        {
+            Turtle.Angle = 0;
+            Turtle.Move(size / 2);
+            WriteO(size / 2);
+        }
+       
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 9;
+
+            // Буква "Т" начало
+            Turtle.X = 200;
+            Turtle.Y = 200;
+            WriteT(60);
+
+            // Буква "о" начало
+            Turtle.X = 230;
+            Turtle.Y = 200;
+            WriteO(30);
+
+            // Буква "р" начало
+            Turtle.X = 270;
+            Turtle.Y = 200;
+            WriteR(30);
+
+            // Буква "Т" маленькая, начало
+            Turtle.X = 302.5;
+            Turtle.Y = 200;
+            WriteT(30);
+
+            Turtle.X = 375;
+            Turtle.Y = 200;
+
+
+
+
+            //PRO 1 DZ
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    Turtle.Move(20);
+            //    Turtle.TurnRight();
+            //    Turtle.Move(20);
+            //    Turtle.TurnRight();
+            //    Turtle.Move(20);
+            //    Turtle.TurnLeft();
+            //    Turtle.Move(20);
+            //    Turtle.TurnLeft();
+            //}
+            //ДЗ-1-4 урок
+            //while (i < 4)
+            //{
+            //    Turtle.Move(20);
+            //    Turtle.TurnRight();
+            //    Turtle.Move(20);
+            //    Turtle.TurnRight();
+            //    Turtle.Move(20);
+            //    Turtle.TurnLeft();
+            //    Turtle.Move(20);
+            //    Turtle.TurnLeft();
+            //    i++;
+            //}
+            // дз-2-4 урок
+            //while (i < 6)
+            //{
+            //    Turtle.Move(120);
+            //    Turtle.Turn(60);
+            //    i++;
+            //}
+        }
+    }
+}
